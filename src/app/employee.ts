e41489940bf73bbd2aff49a12181ec48eb0e7789
Erikas <ErikasGiedraitis@gmail.com>
2021-04-17 19:19:06 +0300

    Connected front-end with back-end server

---
+++ src/app/employee.ts
@@ -0,0 +1,10 @@
+export interface Employee {
+    id: number;
+    name: string;
+    email: string;
+    jobTitle: string;
+    phoneNum: string;
+    imageUrl: string;
+    employeeCode: string;
+  }
+  
